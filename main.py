9cb867c459137bf484c57cab2181b43fa982572d
lages <lagestan@gmail.com>
2021-05-28 00:24:44 +0300

    Init

---
+++ main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+kek = 0
+bek = 0
+check = 0
+while bek < 20:
+    print("q",end="")
+    bek += 1
+    while kek < 10:
+        print()
+        print("q",end="                  q")
+        kek += 1
+    print()
+while check < 20:
+    print("q",end="")
+    check += 1
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
